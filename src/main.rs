de41fdfdfee56c62912ffd763d00a6c550aafa82
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-19 15:40:13 +0000

    feat: add the commands enum to the cli struc.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ mod commands;
 
 use clap::Parser;
 
+use crate::commands::Commands;
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Cli {}
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
 fn main() {
     let _cli = Cli::parse();
